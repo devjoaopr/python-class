b127eba58fcb499f5c61c79d29a981ab944d3fe2
devjoaopr <joaoprsantos1@outlook.com>
2026-09-04 20:13:18 -0300

    describing functions

--- aula_21_08/main.py
+++ aula_21_08/main.py
@@ -1,11 +1,21 @@
 from pedidos import cadastrarClientes, cadastrarProdutos, emitirRecibo
 from pedidosRefatoracao import calcularTotal
+from descricaoPedido import descrever_pedido
 
 try:
+    """Cadastrar cliente e produtos"""
     cliente = cadastrarClientes()
     produtos = cadastrarProdutos()
+
+    """ Calcular subtotal e total do pedido, descrever o pedido e emitir o recibo """
     valores = [p["valor"] for p in produtos]
     subtotal, total = calcularTotal(valores)
+
+    """ Descrever o pedido """
+    descrever_pedido(cliente, produtos=produtos, subtotal=subtotal, total=total)
+
+    """ Emitir recibo do pedido"""
     emitirRecibo(cliente, produtos, subtotal, total)
+
 except (ValueError, TypeError) as error:
     print(f"Ocorreu um erro: {error}")
